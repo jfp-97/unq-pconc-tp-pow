2f9cdeb5d2df04e89f85c9914e197fd0cfdad1db
Juan Francisco <juanf.perez.97@gmail.com>
2020-10-24 11:06:53 -0300

    terminada UnidadDeTrabajo

--- src/unidadDeTrabajo/UnidadDeTrabajo.java
+++ src/unidadDeTrabajo/UnidadDeTrabajo.java
@@ -3,41 +3,53 @@ package unidadDeTrabajo;
 public class UnidadDeTrabajo {
 
 	private int id;
-	private int threadsTotales;
-	private int tamanioNonce;
+	private long combinacionesPosibles;
+	private long combinacionesPorUnidad;
+	private long combinacionesRestantes;
 
-	public UnidadDeTrabajo(int id, int threadsTotales, int tamanioNonce) {
+	public UnidadDeTrabajo(int id, int cantThreads, int tamanioNonce) {
 		this.id = id;
-		this.threadsTotales = threadsTotales;
-		this.tamanioNonce = tamanioNonce;
+		this.combinacionesPosibles = (long) Math.pow(256, tamanioNonce);
+		this.combinacionesPorUnidad = (long) Math.floor(this.combinacionesPosibles / cantThreads);
+		this.combinacionesRestantes = this.combinacionesPosibles % cantThreads;
+	}
+
+	private int getId() {
+		return this.id;
+	}
+
+	private long getCombinacionesPosibles() {
+		return this.combinacionesPosibles;
+	}
+
+	private long getCombinacionesPorUnidad() {
+		return this.combinacionesPorUnidad;
+	}
+
+	private long getCombinacionesRestantes() {
+		return this.combinacionesRestantes;
 	}
 
 	public long rangoMin() {
-		return (long) Math.floor(
-			(this.desplazamiento() + this.id * (Math.pow(256, this.tamanioNonce)) / this.threadsTotales) - 1
-		);
+		return this.desplazamiento(this.getId()) + this.getId() * this.getCombinacionesPorUnidad();
 	}
 
 	public long rangoMax() {
-		return (long) Math.floor(
-			(this.desplazamiento() + (this.id + 1) * (Math.pow(256, this.tamanioNonce)) / this.threadsTotales) - 1
-		);
+		return this.desplazamiento(this.getId() + 1) + (this.getId() + 1) * this.getCombinacionesPorUnidad() - 1;
 	}
 
-	private long desplazamiento() {
-		return this.id > (256 ^ this.tamanioNonce) % this.threadsTotales
-			? 0
-			: this.id + 1;
+	private long desplazamiento(int id) {
+		return Math.min(id, this.getCombinacionesRestantes());
 	}
 
 	public static void main(String[] args) {
-		int cantThreads = 8;
+		int cantThreads = 10;
 		int tamNonce = 4;
 		UnidadDeTrabajo[] us = new UnidadDeTrabajo[cantThreads];
 
 		for(int i = 0; i < cantThreads; i++) {
 			us[i] = new UnidadDeTrabajo(i, cantThreads, tamNonce);
-			System.out.println("Udt " + i + ": [" + us[i].rangoMin() + ", " + us[i].rangoMax() + "]");
+			System.out.println("U. d. t. " + i + ": [" + us[i].rangoMin() + ", " + us[i].rangoMax() + "]");
 		}
 
 
